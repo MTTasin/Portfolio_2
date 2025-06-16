0b81dbbdb6b7e4b45f6774d48fb42141c6f64b26
M.T. Tasin <m.t.tasin2002@gmail.com>
2025-06-16 20:06:54 +0600

    correct the css

--- portfolio_backend/portfolio_backend/settings.py
+++ portfolio_backend/portfolio_backend/settings.py
@@ -125,9 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'media/static/'
-# STATIC_FILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
+STATIC_FILES_DIRS = [
+    os.path.join(BASE_DIR, 'media/static')
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
